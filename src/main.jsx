03fcc5d616ab4c4d0b83b5a17cec946dcf3fd0d8
unknown <kylamaenvaloria@iskolarngbayan.pup.edu.ph>
2025-02-24 22:47:52 +0800

    ci: setup deployment pipeline

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 import "./index.css";
 import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
